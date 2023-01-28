c494c4a251445a0bd937b46d018543bb1a0801c4
Joseph Maa <josephgmaa@berkeley.edu>
2023-01-28 12:45:10 -0800

    Update learn page

--- src/routes/learn/+page.svelte
+++ src/routes/learn/+page.svelte
@@ -1,1 +1,55 @@
-<h1>Learn</h1>
+<script>
+	import compost_banana from '$lib/images/compost_banana.png';
+	import compost_food from '$lib/images/compost_food.jpeg';
+	import compost_sushi from '$lib/images/compost_sushi.png';
+	import compost from '$lib/images/compost.jpeg';
+	import recycle_glass_bottle from '$lib/images/recycle_glass_bottle.jpeg';
+	import recycle_milk from '$lib/images/recycle_milk.jpeg';
+	import recycle_water_bottle from '$lib/images/recycle_water_bottle.jpeg';
+	import recycle from '$lib/images/recycle.jpeg';
+	import trash_bag from '$lib/images/trash_bag.jpeg';
+	import trash_boba_cup from '$lib/images/trash_boba_cup.jpeg';
+	import trash_plastic_bag from '$lib/images/trash_plastic_bag.jpeg';
+	import trash from '$lib/images/trash.jpeg';
+</script>
+
+<h1>Learning Tutorial</h1>
+
+<p>
+	Welcome to the Compost4Kids! In this game, you will learn about the importance of composting and
+	how it can benefit the environment. Your goal is to sort various trash objects into their correct
+	bins: trash, compost, and recycling. Each correct sorting will help you expand and beautify your
+	virtual garden. The more points you earn, the more you can upgrade your garden. Can you become the
+	ultimate composting pro and help create a greener future? Let's get started and compete for the
+	high score!
+</p>
+
+<h2>Trash objects:</h2>
+
+<p>The trash container:</p>
+<img alt="The trash container" src={recycle} />
+
+<p>The trash objects:</p>
+<img alt="." src={trash_bag} />
+<img alt="." src={trash_boba_cup} />
+<img alt="." src={trash_plastic_bag} />
+
+<h2>Recyclable objects:</h2>
+
+<p>The recycle container:</p>
+<img alt="The trash container" src={recycle} />
+
+<p>The recycable objects:</p>
+<img alt="." src={recycle_glass_bottle} />
+<img alt="." src={recycle_milk} />
+<img alt="." src={recycle_water_bottle} />
+
+<h2>Compostable objects:</h2>
+
+<p>The recycle container:</p>
+<img alt="The recycle container" src={recycle} />
+
+<p>The recycable objects:</p>
+<img alt="." src={compost_banana} />
+<img alt="." src={compost_food} />
+<img alt="." src={compost_sushi} />
