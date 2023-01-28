ce08191b90d4b517692a16c3103607d4244d40f0
Aaron Lieberman <aarnliebrmn@gmail.com>
2023-01-28 14:04:20 -0800

    formatting

--- src/lib/Title.svelte
+++ src/lib/Title.svelte
@@ -1,6 +1,6 @@
 <header class="topnav">
-  <a class="active" href="#home">Home</a>
-  <a href="#news">News</a>
-  <a href="#contact">Contact</a>
-  <a href="#about">About</a>
+	<a class="active" href="#home">Home</a>
+	<a href="#news">News</a>
+	<a href="#contact">Contact</a>
+	<a href="#about">About</a>
 </header>
