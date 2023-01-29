d991ef414058dd67e78fd114f44e6840f5cadd51
taylornoh <taylornoh@csu.fullerton.edu>
2023-01-29 02:36:21 -0800

    Added UI to Home and Resources Page.

--- src/lib/Title.svelte
+++ src/lib/Title.svelte
@@ -1,6 +1,32 @@
 <header class="topnav">
-  <a href="/">Home</a>
-  <a href="/learn">Tutorial</a>
-  <a href="/game">Play</a>
-  <a href="/resources">Resources</a>
+  <p id="rcorners2">
+    <a href="/">Home</a>
+    &nbsp;
+    <a href="/learn">Tutorial</a>
+    &nbsp;
+    <a href="/game">Play</a>
+    &nbsp;
+    <a href="/resources">Resources</a>
+  </p>
 </header>
+
+<link href='https://fonts.googleapis.com/css?family=Space Mono' rel='stylesheet'>
+
+<style>
+  header {
+    padding-bottom: 20px;
+    font-family: 'Space Mono';font-size: 22px;
+  }
+
+  a {
+    color: black;
+  }
+
+  #rcorners2 {
+    border-radius: 25px;
+    background: rgba(255, 255, 255, 0.8);
+    padding: 20px;
+    margin-right: 20px;
+    height: auto;
+    }
+</style>
